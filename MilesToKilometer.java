a775667c3d60db9d72d4a8074e4c0bd0fc01036e
gy-host-w <1789859128@qq.com>
2024-09-23 08:21:49 +0800

    Add files via upload

---
+++ MilesToKilometer.java
@@ -0,0 +1,9 @@
+public class MilesToKilometer {
+    public static void main(String[] args) {
+        double miles = 100;
+        final double KILOMETERS_PER_MILES = 1.609;
+
+        double kilometers = miles * KILOMETERS_PER_MILES;
+        System.out.println("The are " + kilometers + " kilometers in " + miles + " miles.");
+    }
+}
