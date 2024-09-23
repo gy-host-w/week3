eb7c6e73195f2a095cc9ad84b51c7a1c82713720
gy-host-w <1789859128@qq.com>
2024-09-23 09:45:23 +0800

    Add files via upload

---
+++ AugmentedOperator.java
@@ -0,0 +1,15 @@
+public class AugmentedOperator {
+    public static void main(String[] args) {
+        double a = 6.5;
+        a = a + a + 6;
+        System.out.println(a);
+
+        double b = 6.5;
+        b = b + 6;
+        System.out.println(b);
+
+        double c = 4;
+        c /= 2;
+        System.out.println(c);
+    }
+}
